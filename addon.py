0ff821861c0a3c1a382ec6c44d4d66b7fa8e76e5
gmdcwork <gmdcwork@gmail.com>
2016-08-27 23:25:13 +0200

    Solved some errors

--- addon.py
+++ addon.py
@@ -9,7 +9,7 @@ addonname   = addon.getAddonInfo('name')
 line1 = "Turned on/off a relay"
 line2 = "Version: " + addon.getAddonInfo('version')
 
-os.system("sh /storage/.kodi/addons/script.relay.master/relay.sh")
+os.system("sh /storage/.kodi/addons/relay-addon-kodi-master/relay.sh")
 
 xbmc.executebuiltin('Notification(Relay Addon, The relay was turned on/off,5000,//storage/.kodi/addons/script.relay.master/icon.png)')
 #xbmcgui.Dialog().ok(addonname, line1, line2)
